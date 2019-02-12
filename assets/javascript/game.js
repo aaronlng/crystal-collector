b3fe2c02966dcf2f86094823013f287099c6d8ae
Aaron L <aaronlng05@gmail.com>
2019-02-12 10:29:38 -0500

    crystal buttons jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 //define the variables to be used
-var crystal1 = "";
-var crystal2 = "";
-var crystal3 = "";
-var crystal4 = "";
 var compNum = Math.floor(Math.random() * ((120-19)+1) + 19);
-
+var crystal1 = Math.floor(Math.random() * ((12-1)+1) + 1);
+var crystal2= Math.floor(Math.random() * ((12-1)+1) + 1);
+var crystal3= Math.floor(Math.random() * ((12-1)+1) + 1);
+var crystal4= Math.floor(Math.random() * ((12-1)+1) + 1);
+var userScore = 0;
 var wins = 0;
 var losses = 0;
 
@@ -14,3 +14,32 @@ $("#wins").text("Wins: " + wins);
 $("#losses").text("Losses: " + losses);
 
 //crystal buttons
+$("#crystal1").on("click", function() {
+    userScore = userScore + crystal1;
+    $("#totalScore").text(userScore);
+});
+
+$("#crystal2").on("click", function() {
+    userScore = userScore + crystal2;
+    $("#totalScore").text(userScore);
+});
+
+$("#crystal3").on("click", function() {
+    userScore = userScore + crystal3;
+    $("#totalScore").text(userScore);
+});
+
+$("#crystal4").on("click", function() {
+    userScore = userScore + crystal4;
+    $("#totalScore").text(userScore);
+});
+
+
+
+console.log(crystal1);
+console.log(crystal2);
+console.log(crystal3);
+console.log(crystal4);
+
+
+
